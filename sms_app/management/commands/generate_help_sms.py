2baea8eb73b9bfe6b7bb86b4548fd8e5160d000b
NinoDoko <ninodokoskiot@hotmail.com>
2016-05-06 10:55:16 +0200

    Generating help templates now has an argument for creating an object instead of just printing the output.

--- sms_app/management/commands/generate_help_sms.py
+++ sms_app/management/commands/generate_help_sms.py
@@ -6,10 +6,16 @@ class Command(BaseCommand):
     help = 'Generates a help message containing all actions that can be received as text.'
 
     def handle(self, *args, **options):
-        auto_replies = [x.received_text for x in MessageTemplateAutoReply.objects.all()]
-        if args: 
-            if args[0] == 'one_line': 
-                help_message = 'Available actions : ' + ', '.join(auto_replies)                
+        auto_replies = [x.received_text for x in MessageTemplateAutoReply.objects.all()] 
+        if 'one_line' in args: 
+            help_message = 'Available actions : ' + ', '.join(auto_replies)
         else: 
             help_message = 'Available actions : \n' + '\n'.join([x + ':' for x in auto_replies])
+        if 'create_object' in args: 
+            try: 
+                help_template = MessageTemplateAutoReply.objects.get(template_title = 'help_template')
+            except Exception: 
+                help_template = MessageTemplateAutoReply(template_title = 'help_template')
+            help_template.template_text = help_message
+            help_template.save()
         return help_message
