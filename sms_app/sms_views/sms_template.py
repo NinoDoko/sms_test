269889b205469e4bc0ae7e844fbbd21fb883efe1
NinoDoko <ninodokoskiot@hotmail.com>
2016-05-20 12:49:08 +0200

    Subscribed users are now automatically checked.

--- sms_app/sms_views/sms_template.py
+++ sms_app/sms_views/sms_template.py
@@ -8,7 +8,11 @@ from sms_actions import query_users_from_get_args
 def view_sms_template(request, sms_id):
     old_template = MessageTemplate.objects.all().get(pk = sms_id)
     template_is_not_reply = old_template.pk not in [x.pk for x in MessageTemplateAutoReply.objects.all()]
-    if not template_is_not_reply: old_template = MessageTemplateAutoReply.objects.all().get(pk = old_template.pk)
+    subscribed_users = []
+    
+    if not template_is_not_reply: 
+        old_template = MessageTemplateAutoReply.objects.all().get(pk = old_template.pk)
+        
     users = Contact.objects.all()
     test_contact = users.all().filter(name = 'Test')[0]
     if request.POST : 
@@ -18,4 +22,3 @@ def view_sms_template(request, sms_id):
         old_template.save()
     users = query_users_from_get_args(request, users)
     return render(request, 'sms_app/view_sms_template.html', {'sms_template' : old_template, 'test_contact' : test_contact, 'queried_users' : users, 'template_is_not_reply' : template_is_not_reply})
-
